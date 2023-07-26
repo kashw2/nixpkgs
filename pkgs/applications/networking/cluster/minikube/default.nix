c12c9c8f6afa44f53319134f59e370eb16127327
kashw2 <supra4keanu@hotmail.com>
2023-07-26 18:15:12 +1000

    f

--- pkgs/applications/networking/cluster/minikube/default.nix
+++ pkgs/applications/networking/cluster/minikube/default.nix
@@ -12,9 +12,9 @@
 
 buildGoModule rec {
   pname = "minikube";
-  version = "1.30.1";
+  version = "1.31.1";
 
-  vendorHash = "sha256-616T47H+8FdXU37MDvAHRyM59JXurU45uz8c/TNxkkc=";
+  vendorHash = "sha256-6vCZUDH35OclO02sV+AXv8+bj4klwoZC0abotheHSoU=";
 
   doCheck = false;
 
