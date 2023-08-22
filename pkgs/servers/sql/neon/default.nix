a20bb76c4240bebed3ff93d432292bb2060522c0
kashw2 <supra4keanu@hotmail.com>
2023-08-22 20:22:19 +1000

    wip

--- pkgs/servers/sql/neon/default.nix
+++ pkgs/servers/sql/neon/default.nix
@@ -1,51 +1,30 @@
-{ lib, rustPlatform, fetchFromGitHub, pkg-config, pkgs }:
+{ 
+stdenv
+, lib
+, rustPlatform
+, fetchFromGitHub
+, pkg-config
+, cmake
+, postgresql_14
+ }:
 
-rustPlatform.buildRustPackage rec {
+stdenv.mkDerivation rec {
     pname = "neon";
-    version = "release-3592";
-
-    preConfigure = ''
-        export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-        export PROTOC="${pkgs.protobuf}/bin/protoc"
-        export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-    '';
-
-LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-
-    nativeBuildInputs = [
-        pkg-config
-        pkgs.glibc
-        pkgs.libxc
-        pkgs.gcc
-        pkgs.clang
-        pkgs.cmake
-    ];
-
-    buildInput = with pkgs; [
-        protobuf
-        openssl.dev
-        flex
-        bison
-        libseccomp
-        llvmPackages.libclang
-    ];
+    version = "3592";
 
     src = fetchFromGitHub {
         owner = "neondatabase";
         repo = pname;
-        rev = version;
+        rev = "release-${version}";
         sha256 = "IC5K3E47St0EizJeP1SjZ9DZnLcIrlOKWza3X/bZTKw=";
+        deepClone = true;
     };
 
-    cargoLock = {
-        lockFile = ./Cargo.lock;
-        outputHashes = {
-            "heapless-0.8.0" = "sha256-phCls7RQZV0uYhDEp0GIphTBw0cXcurpqvzQCAionhs=";
-            "postgres-0.19.4" = "sha256-sX8/e1UE9DhotHRYdz8o/Ui+jgJgzF2wLnTyiXwWdMg=";
-        };
-    };
+    nativeBuildInputs = [ postgresql_14 ];
 
-    cargoHash = lib.fakeHash;
+    buildPhase = ''
+        make
+    '';
 
     meta = with lib; {
         description = "Neon: Serverless Postgres. We separated storage and compute to offer autoscaling, branching, and bottomless storage.";
