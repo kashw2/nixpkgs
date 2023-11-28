ece744e0a477aafe552da8fecdce617b4112333c
kashw2 <supra4keanu@hotmail.com>
2023-11-29 06:50:45 +1000

    xpra: 4.4.6 -> 5.0.3

--- pkgs/tools/X11/xpra/default.nix
+++ pkgs/tools/X11/xpra/default.nix
@@ -68,21 +68,12 @@ let
   '';
 in buildPythonApplication rec {
   pname = "xpra";
-  version = "4.4.6";
+  version = "5.0.3";
 
   src = fetchurl {
     url = "https://xpra.org/src/${pname}-${version}.tar.xz";
-    hash = "sha256-BWf3nypfSrYCzpJ0OfBkecoHGbG1lEgu5jLZhfkIejQ=";
+    hash = "sha256-+pHNCO1tTolQcP6Y8yoEdSMk0FYDmVOE2b5FGyeMdw8=";
   };
-
-  patches = [
-    (substituteAll {  # correct hardcoded paths
-      src = ./fix-paths.patch;
-      inherit libfakeXinerama;
-    })
-    ./fix-41106.patch  # https://github.com/NixOS/nixpkgs/issues/41106
-    ./fix-122159.patch # https://github.com/NixOS/nixpkgs/issues/122159
-  ];
 
   INCLUDE_DIRS = "${pam}/include";
 
